ecb0b8e2840e8e8d5d914110a1cfecd9f438e6b7
Tamara <tech@tamara.co>
2021-03-17 20:19:33 +0100

    Fix configuration product widget

---
+++ view/frontend/web/js/price-box.js
@@ -0,0 +1,28 @@
+define([
+    'jquery',
+    'Magento_Catalog/js/price-utils',
+    'underscore',
+    'mage/template',
+    'jquery/ui'
+], function ($, utils, _, mageTemplate) {
+    'use strict';
+
+    return function (priceBox) {
+        return $.widget('mage.priceBox', priceBox, {
+
+            /**
+             * Render price unit block.
+             */
+            reloadPrice: function reDrawPrices() {
+                this._super();
+
+                //re-render pdp widget
+                if (window.TamaraProductWidget) {
+                    $("#tamara-product-widget").attr("data-price", this.cache.displayPrices.finalPrice.amount);
+                    $("#tamara-product-widget").empty();
+                    window.TamaraProductWidget.render();
+                }
+            },
+        });
+    }
+});
