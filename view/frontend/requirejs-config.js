831be3f37aa0956fab531efed65967f5defa7fd1
Tamara <tech@tamara.co>
2021-03-17 20:21:35 +0100

    Fix configuration product widget

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -3,5 +3,12 @@ var config = {
         "*": {
             tamaraCheckoutFrame: "https://cdn.tamara.co/checkout/checkoutFrame.min.js?v=1.1"
         }
+    },
+    config: {
+        mixins: {
+            'Magento_Catalog/js/price-box': {
+                'Tamara_Checkout/js/price-box': true
+            }
+        }
     }
 };
