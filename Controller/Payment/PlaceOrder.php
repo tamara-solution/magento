834ea152dba9c6a98d53f8a6c410350fd735e8c5
Chien Hoang <chien.hoang@tamara.co>
2022-02-21 10:56:06 +0100

    1.12.22

--- Controller/Payment/PlaceOrder.php
+++ Controller/Payment/PlaceOrder.php
@@ -22,6 +22,11 @@ class PlaceOrder extends Action
     protected $checkoutSession;
 
     /**
+     * @var \Magento\Sales\Api\OrderRepositoryInterface
+     */
+    protected $magentoOrderRepository;
+
+    /**
      * @var \Tamara\Checkout\Model\OrderRepository
      */
     private $tamaraOrderRepository;
@@ -30,10 +35,12 @@ class PlaceOrder extends Action
         \Magento\Framework\App\Action\Context $context,
         JsonFactory $resultJsonFactory,
         CheckoutSession $checkoutSession,
+        \Magento\Sales\Api\OrderRepositoryInterface $magentoOrderRepository,
         \Tamara\Checkout\Model\OrderRepository $tamaraOrderRepository
     ) {
         $this->resultJsonFactory = $resultJsonFactory;
         $this->checkoutSession = $checkoutSession;
+        $this->magentoOrderRepository = $magentoOrderRepository;
         $this->tamaraOrderRepository = $tamaraOrderRepository;
         parent::__construct($context);
     }
@@ -42,12 +49,21 @@ class PlaceOrder extends Action
     {
         $result = $this->resultJsonFactory->create();
         $response = ['success' => true];
-        $lastOrderId = $this->getLastOrderId();
+        $lastOrderId = $this->getRequest()->getParam('orderId', null);
+        if (!$lastOrderId) {
+            $lastOrderId = $this->getLastOrderId();
+        }
         $logger = $this->_objectManager->get('TamaraCheckoutLogger');
         try {
-            $tamaraOrder = $this->tamaraOrderRepository->getTamaraOrderByOrderId($lastOrderId);
-            $response['orderId'] = $lastOrderId;
-            $response['redirectUrl'] = $tamaraOrder->getRedirectUrl();
+            $magentoOrder = $this->magentoOrderRepository->get($lastOrderId);
+            if (substr($magentoOrder->getPayment()->getMethod(), 0, 6) == "tamara"
+                && $magentoOrder->getState() == \Magento\Sales\Model\Order::STATE_NEW) {
+                $tamaraOrder = $this->tamaraOrderRepository->getTamaraOrderByOrderId($lastOrderId);
+                $response['orderId'] = $lastOrderId;
+                $response['redirectUrl'] = $tamaraOrder->getRedirectUrl();
+            } else {
+                throw new NoSuchEntityException(__('Requested order doesn\'t exist'));
+            }
         } catch (NoSuchEntityException $exception) {
             $response['success'] = false;
             $logger->debug(['Tamara - Error when retrieve tamara order: ' => $exception->getMessage()]);
