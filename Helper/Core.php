90347aa7c784214001e23834a7cf51a4f8c5f7b4
Cuong Huynh <cuong.huynh@tamara.co>
2023-11-09 10:12:49 +0400

    2.11.34

--- Helper/Core.php
+++ Helper/Core.php
@@ -231,4 +231,25 @@ class Core extends AbstractHelper
             return $this->storeManager->getStore()->getId();
         }
     }
+
+    /**
+     * @param $str
+     * @return bool
+     */
+    public function isAnUrl($str)
+    {
+        $regex = "((https?|ftp)\:\/\/)?"; // SCHEME
+        $regex .= "([a-z0-9+!*(),;?&=\$_.-]+(\:[a-z0-9+!*(),;?&=\$_.-]+)?@)?"; // User and Pass
+        $regex .= "([a-z0-9-.]*)\.([a-z]{2,3})"; // Host or IP
+        $regex .= "(\:[0-9]{2,5})?"; // Port
+        $regex .= "(\/([a-z0-9+\$_-]\.?)+)*\/?"; // Path
+        $regex .= "(\?[a-z+&\$_.-][a-z0-9;:@&%=+\/\$_.-]*)?"; // GET Query
+        $regex .= "(#[a-z_.-][a-z0-9+\$_.-]*)?"; // Anchor
+
+        if(preg_match("/^$regex$/i", $str)) // `i` flag for case-insensitive
+        {
+            return true;
+        }
+        return false;
+    }
 }
